d064ca3dba40ea0968cdc0f22c9f28db6dc3a8c6
Pronzyt <darkwigduck@gmail.com>
2015-10-21 00:01:03 +0300

    Example 4 added

--- PDE_Solver/Examples.h
+++ PDE_Solver/Examples.h
@@ -5,13 +5,13 @@
 #include <string>
 #include <fstream>
 #include <sstream>
-#include <math>
+#include <math.h>
 #include "Bundle.h"
 
 void runExample1();
 void runExample2();
 void runExample3();
-
+void runExample4();
 
 
 #endif	/* EXAMPLES_H */
