844ba4f347622dcf143f9c1d0265c74842f260d4
PronZyt <darkwigduck@gmail.com>
2016-02-17 17:39:42 +0300

    Range removed, Layer simplified

--- PDE_Solver/core/Layer.h
+++ PDE_Solver/core/Layer.h
@@ -4,101 +4,82 @@
 #include <functional>
 #include "Range.h"
 
-/*Базовый класс, который производит пересчет состояний (State_Ty),
-используя функцию пересчета recount_func. Доступ к диапазону точек осуществляется
-при помощи интерфейса IRange.*/
-
 
 namespace core{
 
 
-template<typename State_Ty>
-class BaseLayer{
-public:
-	typedef BaseLayer<State_Ty> my_Ty;
-	typedef State_Ty value_type;
-	typedef IRange<Range<value_type>, value_type> iterator;
-	typedef std::function<value_type&(iterator&)> recount_func;
-
-	BaseLayer(const BaseLayer& rhs)
-		: m_recount_func(rhs.m_recount_func)
-	{};
-
-	/*Осуществляет шаг пересчета 
-	return true, если конец диапазона был достигнут	*/
-	virtual bool recount_step() = 0; 
-	virtual void recount(){ while (!recount_step()){}; }
-
-	/*	Переход на начало диапазона	*/
-	virtual void reset() = 0;
-	virtual ~BaseLayer() = 0;
-protected:
-	BaseLayer(recount_func func)
-		:m_recount_func(func)
-	{};
-	recount_func m_recount_func;
-};
-
-template<typename Ty>
-BaseLayer<Ty>::~BaseLayer(){};
+//template<typename State_Ty>
+//class BaseLayer{
+//public:
+//	typedef BaseLayer<State_Ty> my_Ty;
+//	typedef State_Ty value_type;
+//
+//	/*Осуществляет шаг пересчета 
+//	return true, если конец диапазона был достигнут	*/
+//	virtual bool recount_step() = 0; 
+//	virtual bool recount() = 0;
+//
+//	/*	Переход на начало диапазона	*/
+//	virtual void reset() = 0;
+//	virtual ~BaseLayer() = 0;
+//};
+//
+//template<typename Ty>
+//BaseLayer<Ty>::~BaseLayer(){};
 
 
 
-template<template<typename>class Range_Ty, typename State_Ty>
-class Layer : public BaseLayer<State_Ty>{
+template<typename Iterator_Ty>
+class Layer /*: public BaseLayer<typename Iterator_Ty::value_type>*/{
 public:
 
-	template<typename BidirectionalIterator>
-	Layer(BidirectionalIterator from, BidirectionalIterator to, int offset, recount_func func)
-		:BaseLayer<State_Ty>(func), m_range(from, to, offset)
+	typedef Iterator_Ty state_iterator;
+
+	typedef std::function<bool(state_iterator&)> recount_func;
+
+	Layer(int offset, recount_func func)
+		: m_offset(offset), m_recount_func(func)
 	{};
 
-	template<typename BidirectionalIterator>
-	Layer(std::pair<BidirectionalIterator, BidirectionalIterator> range, int offset, recount_func func)
-		:Layer(range.first, range.second, offset, func)
+	Layer(Layer&& rhs) :
+		m_begin(std::move(rhs.m_begin)),
+		m_end(std::move(rhs.m_end)),
+		m_offset(std::move(rhs.m_offset)),
+		m_recount_func(std::move( rhs.m_recount_func))
 	{};
 
-	~Layer() override{};
 
-	bool recount_step() override
+	void set_range(state_iterator begin, state_iterator end)
 	{
-		m_range.hold();
-		this->m_recount_func(m_range);
-		m_range.reset();
-		++m_range;
-		return m_range.in_end();
+		std::swap(m_begin, begin);
+		std::swap(m_end, end);
 	};
 
-	void reset() override
+
+	bool recount()
 	{
-		m_range.to_begin();
+		bool result = true;
+		for (state_iterator it = m_begin; it != m_end; ++it)
+		{
+			result = result && m_recount_func(++it);
+		};
+		return result;
 	};
+
 
 private:
 	Layer() = delete;
-	Range_Ty<State_Ty> m_range;
+	Layer& operator= (const Layer& rhs) = delete;
+	
+	state_iterator m_begin;
+	state_iterator m_end;
+	int m_offset;
+	recount_func m_recount_func;
 };
 
 
-template<typename State_Ty>
-class FLayer : public Layer<FRange, State_Ty>{
-public:
-	template <typename... Args>
-	explicit FLayer(Args&&... args)
-		: Layer<FRange, State_Ty>(std::forward<Args>(args)...)
-	{ };
 };
 
 
-template<typename State_Ty>
-class RLayer : public Layer<RRange, State_Ty>{
-public:
-	template <typename... Args>
-	explicit RLayer(Args&&... args)
-		: Layer<RRange, State_Ty>(std::forward<Args>(args)...)
-	{ };
-};
-
-};
 
 #endif	/* LAYER_H */
--- PDE_Solver/core/Stack.h
+++ PDE_Solver/core/Stack.h
@@ -4,45 +4,89 @@
 
 #include "Range.h"
 #include "Layer.h"
-#include <stack>
+#include <functional>
+#include <vector>
 
 namespace core
 {
 
 
 
-template<typename Layer_Ty>
+template<typename Val_Ty>
 class Stack{
 public:
-	typedef Layer_Ty value_type;
+	typedef Val_Ty value_type;
 
-	//template<typename BidirectionalIterator>
-	//Stack(std::pair<BidirectionalIterator, BidirectionalIterator> range)
-	//	:m_range(range.first, range.second)
-	//{};
+	typedef typename  Val_Ty::state_iterator state_iterator;
+	typedef typename state_iterator::value_type state_type;
 
-	template<typename BidirectionalIterator>
-	void set_range(std::pair<BidirectionalIterator, BidirectionalIterator> range)
+
+	Stack(){};
+
+
+	void set_range(state_iterator begin, state_iterator end)
 	{
-		
+		state_iterator begin_local = begin;
+		state_iterator end_local = begin;
+		for (auto it = m_container.begin(); it != m_container.end(); ++it)
+		{
+			std::advance(end_local, it->second + 1);
+			it->first.set_range(begin_local, end_local);
+			std::advance(begin_local, it->second);
+		};		
 	};
+
+
+	bool recount_until(const bool& condition)
+	{
+		bool result = true;
+		while (!condition)
+		{
+			for (auto it = m_container.begin(); it != m_container.end(); ++it)
+			{
+				result = it->first.recount() && result;
+			};
+		};
+		return result;
+	};
+
 
 	void push(size_t size, value_type val)
 	{
-
-		m_stack.push(val);
+		m_container.emplace_back(size, val);
 	};
 
-	void push(){};
 
-	//value_type pop();
-	//recount;
+	template<typename... Args>
+	void push(size_t size, Args&&... args)
+	{
+		m_container.emplace_back(size, Val_Ty(std::forward<Args>(args)...));
+	};
+
+
+	value_type top()
+	{
+		return m_container.back();
+	};
+
+
+	void pop()
+	{
+		return m_container.pop_back();
+	};
+
 
 private:
-	Stack() = delete;
+	typedef std::pair<size_t, value_type> pair_type;
+	typedef std::vector<pair_type> container_type;
 
-	Range<typename Layer_Ty::value_type> m_range;
-	std::stack<value_type> m_stack;
+	Stack(const Stack&) = delete;
+	Stack(Stack&&) = delete;
+	Stack& operator=(const Stack&) = delete;
+
+	state_iterator m_begin;
+	state_iterator m_end;
+	container_type m_container;
 	
 };
 
